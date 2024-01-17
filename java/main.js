4bcf751f319655be6d3a5be87acf1f45e69e65d7
xxdaniquee <xdaniquerijsdijk@gmail.com>
2024-01-17 12:02:27 +0100

    interactie.3

--- java/main.js
+++ java/main.js
@@ -1,11 +1,11 @@
-// const activity = document.querySelector(".activity");
-// const activityOverlay = document.querySelector(".activity-overlay");
+const activity = document.querySelector(".activity");
+const activityOverlay = document.querySelector(".activity-overlay");
 
-// activity.addEventListener('click', () => {
-//   activityOverlay.showModal();
-// });
+activity.addEventListener('click', () => {
+  activityOverlay.showModal();
+});
 
-// const overlayClose = document.querySelector(".overlay-close");
-// overlayClose.addEventListener('click', () => {
-//   activityOverlay.close();
-// });
+const overlayClose = document.querySelector(".overlay-close");
+overlayClose.addEventListener('click', () => {
+  activityOverlay.close();
+});
